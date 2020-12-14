cdf19655a7d3f886e67d29a0a083ba27c10f9040
sdhamani <74397480+sdhamani@users.noreply.github.com>
2020-12-15 02:37:40 +0530

    Implemted prime number logic

--- app.js
+++ app.js
@@ -1,16 +1,53 @@
-import moment from '../moment.js';
+// import moment from 'src/moment.js';
 
-// var moment = require('moment');
+// // var moment = require('moment');
 
 var input = document.querySelector('#input')
 
+
 var button = document.querySelector('#checkBtn');
+
+var displayMsg = document.querySelector('#message');
+
+displayMsg.dis
+
+var isPrime = true;
+
 
 
 function checkYear() {
-    moment(input.value).isValid();
-    console.log(input.value)
-}
+    var array = (input.value).split("-");
+    var birthDay = array[2];
 
+    console.log(birthDay)
+
+    if (birthDay == 1){
+        displayMsg = "1 is neither prime nor composite number";
+
+    }
+
+    else if(birthDay > 1){
+
+        for (var i=2 ; i < birthDay ; i++){
+            if(birthDay % i == 0){
+                displayMsg = "Your BirthDay is not a prime number";
+                isPrime = false;
+                break;
+            }
+        }
+        if(isPrime){
+        displayMsg = "Your BirthDay is a prime number"}
+
+    }
+
+    else{
+        displayMsg = "Your BirthDay is not a prime number";
+    }
+    console.log(displayMsg)
+
+    
+    document.getElementById('message').innerHTML  = displayMsg
+    
+}
 
 button.addEventListener('click', checkYear)
